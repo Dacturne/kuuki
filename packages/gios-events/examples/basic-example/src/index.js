a69b20ec01a8f8fee381801c5326d90e2e41f403
Dawid Cygert <32225868+Dacturne@users.noreply.github.com>
2019-12-15 18:23:18 +0100

    Update sample usage

--- packages/gios-events/examples/basic-example/src/index.js
+++ packages/gios-events/examples/basic-example/src/index.js
@@ -13,17 +13,24 @@ const gios = require("../../../../gios/dist"); // @kuuki/gios
   await eventApi.initialize();
   // any measurements that have never been registered and are not null
   eventApi.on("measurement", (stationId, sensor, measurement) => {
+    // find the station (to gather more metadata)
+    const station = eventApi.findStation(stationId);
     console.log({
+      station: station.identifier.id,
       sensor: sensor.identifier.id,
       val: measurement.value,
+      address: station.raw.addressStreet,
       op: "insert"
     });
   })
   // any measurements that have already been registered, but a change in value has occured
   eventApi.on("measurement_update", (stationId, sensor, measurement) => {
+    const station = eventApi.findStation(stationId);
     console.log({
+      station: station.identifier.id,
       sensor: sensor.identifier.id,
       val: measurement.value,
+      address: station.raw.addressStreet,
       op: "upsert"
     });
   })
