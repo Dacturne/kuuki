aa35d12fa569db7f43c208fbe83543a570e7b12b
Dawid Cygert <32225868+Dacturne@users.noreply.github.com>
2019-12-25 12:59:58 +0100

    Bump up TS version, Ignore map files, Cleanup

--- packages/luftdaten/src/lib/services/FetchService.ts
+++
@@ -1,9 +0,0 @@
-export class FetchService implements IFetch {
-  constructor(private _fetch: any) {
-    //
-  }
-
-  public fetch(url: RequestInfo, init?: RequestInit): Promise<Response> {
-    return this._fetch(url, init);
-  }
-}
