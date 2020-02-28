1c06a98be7111af3c1ab96da19c735f133d1bf64
Dawid Cygert <32225868+Dacturne@users.noreply.github.com>
2020-02-28 14:55:52 +0100

    Update example

--- packages/gios-events/examples/basic-example/src/index.js
+++ packages/gios-events/examples/basic-example/src/index.js
@@ -1,20 +1,20 @@
 const giosEvents = require("../../../dist"); // @kuuki/gios-events
 const gios = require("../../../../gios/dist"); // @kuuki/gios
 
-(async() => {
+(async () => {
   const eventApi = new giosEvents.GiosAirQualityEventsService(
     new gios.GiosAirQualityService(),
     {
-      stations:     "*/15 * * * *", // every 10 minutes
-      sensors:      "*/15 * * * *",
+      stations: "*/15 * * * *", // every 10 minutes
+      sensors: "*/15 * * * *",
       measurements: "*/5 * * * *"
     }
   );
   await eventApi.initialize();
   // any measurements that have never been registered and are not null
-  eventApi.on("measurement", (stationId, sensor, measurement) => {
+  eventApi.on("measurement", async (stationId, sensor, measurement) => {
     // find the station (to gather more metadata)
-    const station = eventApi.findStation(stationId);
+    const station = await eventApi.findStation(stationId);
     console.log({
       station,
       sensor,
@@ -22,9 +22,9 @@ const gios = require("../../../../gios/dist"); // @kuuki/gios
       op: "insert"
     });
   })
-  eventApi.on("measurement_update", (stationId, sensor, measurement) => {
+  eventApi.on("measurement_update", async (stationId, sensor, measurement) => {
     // find the station (to gather more metadata)
-    const station = eventApi.findStation(stationId);
+    const station = await eventApi.findStation(stationId);
     console.log({
       station,
       sensor,
